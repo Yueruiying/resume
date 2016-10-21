9bcf0cd4b073de58f93b35cf17a51ac56f7f25f5
Sunil Kumar Behera <behera.sunil.k@gmail.com>
2016-10-21 14:52:25 +0530

    Completed almost

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,99 +1,149 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
-var name="Sunil Behera";
-var role="Web Developer";
-
-var mobile="+91 94375 95871";
-var email="behera.sunil.k@gmail.com";
-var twitter="_sunilbehera";
-var github="beheraSunil";
-// var blog="NA";
-var mylocation="India";
-
-var bioPic="./images/fry.jpg";
-var welcomeMsg="Get ready to be impressed like never before!!";
-
-var skills=["HTML5", "CSS3", "JS"];
-
-// var workEmployer = "NA";
-// var workTitle = "NA";
-// var workDates = "NA";
-// var workLocation = "NA";
-// var workDescription = "NA";
-
-// var schoolStart = "NA";
-// var schoolName = "NA";
-// var schoolDegree = "NA";
-// var schoolDates = "NA";
-// var schoolLocation = "NA";
-// var schoolMajor = "NA";
-
-var bio={
-    "name" : name,
-    "role" : role,
+var bio = {
+    "name" : "Sunil Behera",
+    "role" : "Web Developer",
     "contacts" : {
-        "mobile": mobile,
-        "email" : email
+        "mobile": "+91 94375 95871",
+        "email" : "behera.sunil.k@gmail.com",
+        "twitter" : "_sunilbehera",
+        "github" :"beheraSunil",
+        "location" : "India",
     },
-    "bioPic" : bioPic,
-    "welcomeMsg" : welcomeMsg,
-    "skills" : skills
+    "bioPic" : "./images/fry.jpg",
+    "welcomeMsg" : "Get ready to be impressed like never before!!",
+    "skills" : ["HTML5", "CSS3", "JS"]
 };
 
-var work={
-    "jobs" :[
+var work = {
+    "jobs" : [
     {
-        "workEmployer" : "Employer Number One",
-        "workTitle":"Work Title One",
-        "workDescription": "Work Description one",
-        "workLocation" : "Work Location One",
-        "workDates" : "Work date One",
-    },
-    {
-        "workEmployer" : "Employer Number two",
-        "workTitle": "Work Title two",
-        "workDescription": "Work Description two",
-        "workLocation" : "Work Location two",
-        "workDates" : "Work date two",
-    }
-    ],
-};
-
-var education={
-    "school": [
-    {
-        "schoolName" : "KVBAM",
-        "city": "BAM"
-    },
-    {
-        "schoolName" : "KVPAN",
-        "city" : "PAN"
+        "workEmployer" : "Upwork",
+        "workTitle":"Freelance Developer",
+        "workDates" : "2016 - Present",
+        "location" : "India",
+        "workDescription": "I work on various projects with different clients and provide technical solutions.",
     }
     ]
 };
-var projects={
-    "myproject" : [
+
+var education = {
+    "schools": [
     {
-        "name" : "name_pt1",
-        "date" : "date_pt1",
-    }]
+        "schoolName" : "National Institute of Science & Technology",
+        "schoolDegree" : "Bachelor of Technology",
+        "schoolDates" : "2016",
+        "location" : "Berhampur, Odisha",
+        "schoolMajor" : "Computer Science & Engineering",
+    },
+    {
+        "schoolName" : "Kendriya Vidalaya",
+        "schoolDegree" : "High School",
+        "schoolDates" : "2012",
+        "location" : "India",
+        "schoolMajor" : "Intermediate Science, Mathematics & Computer Science",
+    }
+    ],
+    "courses" : [
+    {
+        "onlineTitle" : "Front-End Web Developer, Naaodegree",
+        "onlineSchool" : "Udacity",
+        "onlineDates" : "2016",
+        "onlineURL" : "http://udacity.com",
+    }
+    ]
+};
+
+var projects = {
+    "allProjects" : [
+    {
+        "projectTitle" :"Online Resume",
+        "projectDates" :"Oct 2016",
+        "projectDescription" :"An online resume(the one you are on) developed with Javascript",
+        "projectImage" :"./images/fry.jpg",
+    },
+    {
+        "projectTitle" :"Portfolio",
+        "projectDates" :"Oct 2016",
+        "projectDescription" :"A responsive website to showcase all my work.",
+        "projectImage" :"./images/fry.jpg",
+    }
+    ]
 }
-projects.display = function()
+
+bio.display=function()
 {
-    for(project in projects.myproject)
+    $("#header").prepend(HTMLheaderRole.replace("%data%",bio.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+
+    $("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+    $("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+    $("#topContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+    $("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+    $("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+    $("#header").append(HTMLbioPic.replace("%data%",bio.bioPic));
+    $("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMsg));
+
+    $("#header").append(HTMLskillsStart);
+    for(skill in bio.skills)
     {
-        $("#main").append(HTMLprojectStart);
-        $("#main").append(HTMLprojectTitle.replace("%data%",projects.myproject[project].name));
-        $("#main").append(HTMLprojectDates.replace("%data%",projects.myproject[project].date));
+        $("#skills-h3").append(HTMLskills.replace("%data%",bio.skills[skill]));
     }
 }
+
+work.display=function()
+{
+    for(job in work.jobs)
+    {
+        var formattedWorkEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].workEmployer);
+        var formattedWorkTitle = HTMLworkTitle.replace("%data%",work.jobs[job].workTitle);
+        var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+        var formattedWorkDescription = HTMLworkDescription.replace("%data%",work.jobs[job].workDescription);
+        var formattedWorkDates = HTMLworkDates.replace("%data%",work.jobs[job].workDates);
+
+        $("#workExperience").append(HTMLworkStart);
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(formattedWorkDescription);
+        $(".work-entry:last").append(formattedWorkDates);
+        $(".work-entry:last").append(formattedWorkLocation);
+    }
+}
+
+education.display=function()
+{
+    for(school in education.schools)
+    {
+        $("#education").append(HTMLschoolStart);
+
+        $(".education-entry:last").append(HTMLschoolName.replace("%data%",education.schools[school].schoolName) + HTMLschoolDegree.replace("%data%",education.schools[school].schoolDegree));
+
+        $(".education-entry:last").append(HTMLschoolDates.replace("%data%",education.schools[school].schoolDates));
+        $(".education-entry:last").append(HTMLschoolLocation.replace("%data%",education.schools[school].location));
+        $(".education-entry:last").append(HTMLschoolMajor.replace("%data%",education.schools[school].schoolMajor));
+    }
+
+
+}
+
+projects.display = function()
+{
+    for(project in projects.allProjects)
+    {
+        $("#projects").append(HTMLprojectStart);
+        $(".project-entry:last").append(HTMLprojectTitle.replace("%data%",projects.allProjects[project].projectTitle));
+        $(".project-entry:last").append(HTMLprojectDates.replace("%data%",projects.allProjects[project].projectDates));
+        $(".project-entry:last").append(HTMLprojectDescription.replace("%data%",projects.allProjects[project].projectDescription));
+        $(".project-entry:last").append(HTMLprojectImage.replace("%data%",projects.allProjects[project].projectImage));
+    }
+
+}
+
 $(document).click(function(loc) {
-  // your code goes here
-  var x=loc.pageX;
-  var y=loc.pageY;
-  // console.log(loc.y);
+  var y=loc.pageY
+  var x=loc.pageX
   logClicks(x,y);
 });
 
@@ -106,54 +156,9 @@ function InName(Name) {
     return finalName;
 };
 
-$("#header").append(HTMLheaderName.replace("%data%",name));
-$("#header").append(HTMLheaderRole.replace("%data%",role));
-
-
-// $("#header").append(HTMLcontactGeneric);
-$("#header").append(HTMLmobile.replace("%data%",mobile));
-$("#header").append(HTMLemail.replace("%data%",email));
-$("#header").append(HTMLtwitter.replace("%data%",twitter));
-$("#header").append(HTMLgithub.replace("%data%",github));
-// $("#header").append(HTMLblog.replace("%data%",blog));
-$("#header").append(HTMLlocation.replace("%data%",mylocation));
-
-
-
-$("#header").append(HTMLbioPic.replace("%data%",bioPic));
-
-$("#header").append(HTMLwelcomeMsg.replace("%data%",welcomeMsg));
-
-
-
-if(bio.skills.length!=0)
-{
-    $("#header").append(HTMLskillsStart);
-    var formattedSkills=HTMLskills.replace("%data%",skills[0]);
-    $("#header").append(formattedSkills);
-
-    var formattedSkills=HTMLskills.replace("%data%",skills[1]);
-    $("#header").append(formattedSkills);
-
-    var formattedSkills=HTMLskills.replace("%data%",skills[2]);
-    $("#header").append(formattedSkills);
-}
-
-for(job in work.jobs){
-    $("#main").append(HTMLworkStart);
-    var formattedWorkEmployer=HTMLworkEmployer.replace("%data%",work.jobs[job].workEmployer);
-    $("#main").append(formattedWorkEmployer);
-    var formattedWorkTitle=HTMLworkTitle.replace("%data%",work.jobs[job].workTitle);
-    $("#main").append(formattedWorkTitle);
-     var formattedWorkDescription=HTMLworkDescription.replace("%data%",work.jobs[job].workDescription);
-    $("#main").append(formattedWorkDescription);
-     var formattedWorkLocation=HTMLworkLocation.replace("%data%",work.jobs[job].workLocation);
-    $("#main").append(formattedWorkLocation);
-     var formattedWorkDates=HTMLworkDates.replace("%data%",work.jobs[job].workDates);
-    $("#main").append(formattedWorkDates);
-
-    // var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle
-    // $(".work-entry:last").append(formattedEmployerTitle);
-}
+bio.display();
+work.display();
 projects.display();
+education.display();
+$("#main").append(googleMap);
 $("#main").append(internationalizeButton);
