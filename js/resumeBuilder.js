031d9d4afc95c6f0b7d3515886260f14eeae598a
Sunil Kumar Behera <behera.sunil.k@gmail.com>
2016-10-23 13:24:53 +0530

    Fixed all the major issues

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,8 +19,9 @@ var bio = {
         "linkedin": "beherasunil",
         "location": "India",
     },
-    "bioPic": "./images/fry.jpg",
-    "welcomeMsg": "Proceed With Caution!!",
+
+    "bioPic": "./images/myFace.png",
+    "welcomeMsg": ["Welcome!", "Namaste!", "Hola!", "Bonjour!", "Ciao!", "Ola!", "Salaam!"],
     "skills": [html5, css3, js, jquery, bootstrap, git, npm, grunt]
 };
 
@@ -28,6 +29,7 @@ var work = {
     "jobs": [{
         "workEmployer": "Upwork",
         "workTitle": "Freelance Developer",
+        "link": "https://www.upwork.com/",
         "workDates": "2016 - Present",
         "location": "Berhampur",
         "workDescription": "I work on various projects with different clients and provide technical solutions.",
@@ -41,12 +43,14 @@ var education = {
         "schoolDates": "2016",
         "location": "Berhampur, Odisha",
         "schoolMajor": "Computer Science & Engineering",
+        "link": "http://nist.edu/",
     }, {
         "schoolName": "Kendriya Vidalaya",
         "schoolDegree": "High School",
         "schoolDates": "2012",
         "location": "Panvel,Navi Mumbai",
         "schoolMajor": "Intermediate Science, Mathematics & Computer Science",
+        "link": "http://kvsangathan.nic.in/",
     }],
     "courses": [{
         "onlineTitle": "Front-End Web Developer, Naaodegree",
@@ -62,11 +66,13 @@ var projects = {
         "projectDates": "Oct 2016",
         "projectDescription": "An online resume(the one you are on) developed with Javascript",
         "projectImage": "./images/fry.jpg",
+        "link": "#",
     }, {
         "projectTitle": "Portfolio",
         "projectDates": "Oct 2016",
         "projectDescription": "A responsive website to showcase all my work.",
         "projectImage": "./images/fry.jpg",
+        "link": "https://beherasunil.github.io/Portfolio/",
     }]
 };
 
@@ -81,23 +87,22 @@ bio.display = function() {
     $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
 
     $("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
-    $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg));
-
+    $("#header").append(HTMLwelcomeMsg.replace("%data%", " "));
     $("#header").append(HTMLskillsStart);
     for (skill = 0; skill < bio.skills.length; skill++) {
         $("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
     }
+    contactLink();
 };
 
 work.display = function() {
     for (job = 0; job < work.jobs.length; job++) {
         var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].workEmployer);
         var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].workTitle);
-        var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+        var formattedEmployerTitle = formattedWorkEmployer.replace("#", work.jobs[job].link) + formattedWorkTitle;
         var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
         var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].workDescription);
         var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].workDates);
-
         $("#workExperience").append(HTMLworkStart);
         $(".work-entry:last").append(formattedEmployerTitle);
         $(".work-entry:last").append(formattedWorkDescription);
@@ -110,7 +115,7 @@ education.display = function() {
     for (school = 0; school < education.schools.length; school++) {
         $("#education").append(HTMLschoolStart);
 
-        $(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[school].schoolName) + HTMLschoolDegree.replace("%data%", education.schools[school].schoolDegree));
+        $(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[school].schoolName).replace("#", education.schools[school].link) + HTMLschoolDegree.replace("%data%", education.schools[school].schoolDegree));
 
         $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school].schoolDates));
         $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[school].location));
@@ -119,7 +124,7 @@ education.display = function() {
     $("#education").append(HTMLonlineClasses);
     for (course = 0; course < education.courses.length; course++) {
         $("#education").append(HTMLschoolStart);
-        $(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.courses[course].onlineTitle) + HTMLonlineSchool.replace("%data%", education.courses[course].onlineSchool));
+        $(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.courses[course].onlineTitle).replace("#", education.courses[course].onlineURL) + HTMLonlineSchool.replace("%data%", education.courses[course].onlineSchool));
 
         $(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.courses[course].onlineDates));
         $(".education-entry:last").append(HTMLonlineURL.replace("%data%", education.courses[course].onlineURL));
@@ -130,13 +135,16 @@ education.display = function() {
 projects.display = function() {
     for (project = 0; project < projects.allProjects.length; project++) {
         $("#projects").append(HTMLprojectStart);
-        $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.allProjects[project].projectTitle));
+        $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.allProjects[project].projectTitle).replace("#", projects.allProjects[project].link));
         $(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.allProjects[project].projectDates));
         $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.allProjects[project].projectDescription));
         // $(".project-entry:last").append(HTMLprojectImage.replace("%data%",projects.allProjects[project].projectImage));
     }
 
 };
+$("#mapDiv").append(googleMap);
+$("#main").append(internationalizeButton);
+$("#main").append(gototop);
 
 $(document).click(function(loc) {
     var y = loc.pageY;
@@ -176,26 +184,21 @@ var contactLink = function() {
         $(this).css('cursor', 'pointer');
     });
 };
-var pop = function() {
-    //var el =
-    $(".gray").hover(function() {
-        $(this).css("box-shadow", "0px 13px 23px 1px rgba(115,101,115,1)");
-    });
-    $(".gray").mouseleave(function() {
-        $(this).css("box-shadow", "0px 12px 15px 1px rgba(170,175,176,1)");
-    });
-};
-var connect = function() {
-    $("#linked").click(function() {
-        window.open('https://in.linkedin.com/in/' + bio.contacts.linkedin, "_blank");
-    });
-    $("#linked").hover(function() {
-        $(this).css('cursor', 'pointer');
-    });
-};
-$("#mapDiv").append(googleMap);
-$("#main").append(internationalizeButton);
-$("#main").append(gototop);
+
+$(".gray").hover(function() {
+    $(this).css("box-shadow", "0px 13px 23px 1px rgba(115,101,115,1)");
+});
+$(".gray").mouseleave(function() {
+    $(this).css("box-shadow", "0px 12px 15px 1px rgba(170,175,176,1)");
+});
+$("h2").hover(function() {
+    $(this).css("color", "black");
+});
+$("h2").mouseleave(function() {
+    $(this).css("color", "gray");
+});
+
+
 $("#tp").click(function() {
     $('body, html').animate({
         scrollTop: 0
@@ -210,9 +213,27 @@ $("#internationalizeButton").click(function() {
     }, 800);
 });
 
+$("#lets-connect").click(function() {
+    window.open('https://in.linkedin.com/in/beherasunil', "_blank");
+
+});
+$("#lets-connect").hover(function() {
+    $(this).css('cursor', 'pointer');
+});
+
 bio.display();
 work.display();
 projects.display();
 education.display();
-contactLink();
-pop();
+
+var counter = 0;
+var elem = document.getElementById("changeText");
+setInterval(change, 1900);
+
+function change() {
+    elem.innerHTML = bio.welcomeMsg[counter];
+    counter++;
+    if (counter >= bio.welcomeMsg.length) {
+        counter = 0;
+    }
+}
